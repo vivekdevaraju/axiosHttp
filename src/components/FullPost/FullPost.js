c2bc6a7d23212bdf44e3cf515e435d710c675c2d
vivekdevaraju <vivekdevaraju@gmail.com>
2018-05-07 10:11:33 +0530

    Post Calls

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './FullPost.css';
 
 class FullPost extends Component {
+
+    state = {
+        loadedComponent : null
+    }
+    componentDidUpdate(){
+        if(this.props.id){
+            axios.get('https://jsonplaceholder.typicode.com/posts' + this.props.id)
+            .then(response => {
+                this.setState({loadedComponent :response.data});
+            });
+        }
+    }
     render () {
-        let post = <p>Please select a Post!</p>;
+        let post = <p style ={{textAlign : 'center'}}>Please select a Post!</p>;
+        if(this.props.id){
         post = (
             <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
+                <h1>{this.state.loaded.title}</h1>
+                <h1>{this.props.author}</h1>
+                <p>{this.props.content}</p>
                 <div className="Edit">
-                    <button className="Delete">Delete</button>
+                    <button onClick = {this.props.deleted} className="Delete">Delete</button>
                 </div>
             </div>
 
         );
+        }
         return post;
     }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+state = {
+    posts :[],
+    selectedPostId :null
+}
+
+
+    postClickedHandler = (id) => {
+        const post = this.state.posts[this.state.posts.findIndex(p=>{return p.id===id;})];
+        this.setState({selectedPost : post});
+    }
+
+
+    deleteClickedHandler = (id) => {
+        const posts = [...this.state.posts];
+        console.log(id);
+        posts.splice([this.state.posts.findIndex(p=>{return p.id===id;})],1);
+        this.setState({posts : posts});
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            const post = response.data.splice(0,4).map(resp => {
+                return {...resp,
+                    author : 'Vivek'
+                }          
+            });
+            this.setState({posts : post});
+            console.log(response.data);
+        });
+    }
     render () {
+        const posts = this.state.posts.map(post => {
+            return<Post 
+            key = {post.id}
+             title = {post.title}
+              author = {post.author}
+               clicked = {() =>{this.postClickedHandler(post.id)}} />
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                   {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id ={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
