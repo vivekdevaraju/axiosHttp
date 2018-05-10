dcdc0ef6607783bbd35fb90656c1786a42e6bdab
vivekdevaraju <vivekdevaraju@gmail.com>
2018-05-10 10:40:39 +0530

    Commit

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -45,15 +42,18 @@ state = {
                clicked = {() =>{this.postClickedHandler(post.id)}} />
         });
         return (
-            <div>
+            <div className = "Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href = "/">Home</a></li>
+                            <li><a href = "/new-post">New Post</a></li>
+
+                        </ul>
+                    </nav>    
+                </header>
                 <section className="Posts">
                    {posts}
-                </section>
-                <section>
-                    <FullPost id ={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
                 </section>
             </div>
         );
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Posts extends Component {
+    render() {
+        return();
+    }
+}
+
+export default Posts;
