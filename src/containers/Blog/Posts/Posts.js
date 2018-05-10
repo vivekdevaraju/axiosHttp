7d5d4c93ad4db6873d7f6b00cfb1c2df3d899d96
vivekdevaraju <vivekdevaraju@gmail.com>
2018-05-10 10:41:21 +0530

    Commit

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 
 class Posts extends Component {
     render() {
+        const posts = this.state.posts.map(post => {
+            return<Post 
+            key = {post.id}
+             title = {post.title}
+              author = {post.author}
+               clicked = {() =>{this.postClickedHandler(post.id)}} />
+        });
         return();
     }
 }
